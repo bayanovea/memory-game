cf37d3a6fa2e2c3ad260fa7f901e06ac9eea8b21
evgeniy.bayanov <e.bayanov@skyeng.ru>
2021-09-21 01:34:09 +0300

    Add main part

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -5,5 +5,61 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
+  let token
+
+  before(async () => {
+    token = await MemoryToken.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = token.address
+
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it ('has a name', async () => {
+      const name = await token.name()
+
+      assert.equal(name, "Memory Token")
+    })
+
+    it ('has a symbol', async () => {
+      const symbol = await token.symbol()
+
+      assert.equal(symbol, "MEMORY")
+    })
+  })
+
+  describe('token distirbution', async () => {
+    let result
+
+    it('monts token', async () => {
+      await token.mint(accounts[0], 'https://cdn-icons-png.flaticon.com/512/4982/4982415.png')
+
+      const totalSupply = await token.totalSupply();
+      assert.equal(totalSupply.toString(), '1', 'total supply is correct')
+
+      const balance = await token.balanceOf(accounts[0])
+      assert.equal(balance.toString(), '1', 'balanceOf is correct')
+
+      const owner = await token.ownerOf('1')
+      assert.equal(owner.toString(), accounts[0].toString(), 'ownerOf of is correct')
+
+      let balanceOf = await token.balanceOf(accounts[0])
+      let tokenIds = []
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.tokenOfOwnerByIndex(accounts[0], i)
+        tokenIds.push(id.toString())
+      }
+      let expected = ['1']
+      assert.equal(tokenIds.toString(), expected.toString(), 'tokenIds are correct')
+
+      let tokenURI = await token.tokenURI('1')
+    assert.equal(tokenURI, 'https://cdn-icons-png.flaticon.com/512/4982/4982415.png', 'tokenURI is correct')
+    })
+  })
 })
